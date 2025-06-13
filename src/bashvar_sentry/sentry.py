836a99e419f60577c7fb5032031ada3f7c0ef257
involution <involution@gmail.com>
2025-06-13 09:29:07 -0400

    fix looped import

--- src/bashvar_sentry/sentry.py
+++ src/bashvar_sentry/sentry.py
@@ -6,7 +6,6 @@ BashVar Sentry: Safely source bash scripts and extract variables.
 
 __version__ = "1.0.0"
 
-from .sentry import source_and_get_vars
 from .exceptions import (
     BashVarSentryError,
     BashScriptError,
