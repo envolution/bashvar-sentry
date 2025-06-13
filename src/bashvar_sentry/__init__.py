d1cd7f23147c72d8ae5b4c2e5e274b0990b99845
involution <involution@gmail.com>
2025-06-13 09:34:11 -0400

    reorder import

--- src/bashvar_sentry/__init__.py
+++ src/bashvar_sentry/__init__.py
@@ -6,13 +6,13 @@ BashVar Sentry: Safely source bash scripts and extract variables.
 
 __version__ = "1.0.0"
 
-from .sentry import source_and_get_vars
 from .exceptions import (
     BashVarSentryError,
     BashScriptError,
     ScriptNotFoundError,
     ParsingError,
 )
+from .sentry import source_and_get_vars
 
 __all__ = [
     "source_and_get_vars",
